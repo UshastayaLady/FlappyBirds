39ca3db2e531839498f32a2488a33752eac1bc55
В3441_МагераМА <1arya11stark@gmail.com>
2026-03-30 23:58:06 +0300

    Добавила колонны, поменяла тригер на колизию

--- Assets/Scripts/GameOverZone.cs
+++ Assets/Scripts/GameOverZone.cs
@@ -1,18 +1,30 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class GameOverPlayer : MonoBehaviour
 {
     public static event Action gameOver;
-
     
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnEnable()
     {
-        if (other.gameObject.GetComponent<FlyingPlayer>())
+        gameOver += DisableScript;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<FlyingPlayer>())
         {
-            gameOver?.Invoke();
-            Time.timeScale = 0;
-            this.gameObject.SetActive(false);
+            gameOver?.Invoke();            
         }
+    }
+   
+    private void DisableScript()
+    {
+        gameObject.SetActive(false);
+    }
+    private void OnDisable()
+    {
+        gameOver -= DisableScript;
     }
 }
